d091da25a5b286b21bee53996cf94b8af9d7bd48
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2024-08-08 13:09:42 -0700

    add exchange/topic input

--- scripts/send_2fa.py
+++ scripts/send_2fa.py
@@ -15,29 +15,24 @@ def new_access_token(refresh_token=None):
 
     try:
         if refresh_token:
-            # Use the refresh token to get a new access token
             token = keycloak_openid.refresh_token(refresh_token)
         else:
-            # Get initial token with required scopes
             token = keycloak_openid.token(grant_type='password', 
                                           username=username, 
                                           password=password, 
                                           totp=otp, 
                                           scope='openid rabbitmq.read:*/* rabbitmq.write:*/* rabbitmq.configure:*/*')
 
-        # Check if the request was successful
         if 'access_token' in token:
             print(f"\nAccess token retrieved successfully: {token['access_token']}\n")
             print(f"Access token scope: {token['scope']}\n")
             return token['access_token'], token['refresh_token']
         else:
-            # Raise an error if the request was unsuccessful
             raise Exception("Error: The request was unsuccessful.")
     except Exception as e:
         print(f"An error occurred: {e}")
         raise
 
-# Define credentials
 global client_id, username, password, client_secret_key, otp
 client_id = input("Enter client ID: ")
 username = input("Enter your username: ")
@@ -45,15 +40,13 @@ password = getpass.getpass("Enter your password: ")
 client_secret_key = input("Enter client secret key: ")
 otp = input("Enter OTP: ")
 
-# Retrieve the access token and refresh token
 access_token, refresh_token = new_access_token()
 
-# Use the access token as the password for RabbitMQ
-credentials = pika.PlainCredentials('', access_token)  # Use the correct username
+credentials = pika.PlainCredentials('', access_token)
 try:
     connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost',
-        virtual_host="/",  # URL-encoded virtual host
+        virtual_host="/",
         port=5672,
         credentials=credentials))
     print("Connection established successfully.")
@@ -62,8 +55,13 @@ except pika.exceptions.ProbableAccessDeniedError as e:
     sys.exit(1)
 
 main_channel = connection.channel()
-main_channel.queue_declare(queue="keycloak", auto_delete=False, durable=True,
-                           arguments={"x-queue-type": "quorum"})
+
+# Get the exchange name and topic or queue name from the user
+exchange_name = input("Enter the exchange name: ")
+topic_or_queue = input("Enter the topic or queue name (e.g., topic.topic2.topic3): ")
+
+# Declare the topic exchange
+main_channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
 
 _COUNT_ = 100
 
@@ -75,17 +73,15 @@ for i in range(0, _COUNT_):
     message = f'Message #{i}'
     print(f'[x] Sent: {message}')
 
-    # Update the secret each minute. Supposed that Access Token Lifespan is 1 minute.
     if (datetime.datetime.now() - last_update).total_seconds() > 55:
         last_update = datetime.datetime.now()
-        # print('updating secret {}'.format((datetime.datetime.now() - tnow).total_seconds() / 60))
         print('Refreshing token.')
         access_token, refresh_token = new_access_token(refresh_token)
         connection.update_secret(access_token, 'secret')
 
     main_channel.basic_publish(
-        exchange='',
-        routing_key='keycloak',
+        exchange=exchange_name,
+        routing_key=topic_or_queue,
         body=message,
         properties=pika.BasicProperties(content_type='application/json'))
 
